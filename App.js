778c2d934d908edb2d7a569be27e06fd40cf2dbe
杉山裕紀 <>
2020-08-13 22:19:54 +0900

    initial comit

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  _onPress = () => {
+    Alert.alert("ボタンをクリックしました");
+    console.log('click now');
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="button"
+              onPress={() => Alert.alert("click now")}/>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +26,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:  '#fff',
   },
 });
