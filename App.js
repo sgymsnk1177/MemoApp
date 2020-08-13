8a2b7278a2becec71ecfdf973a7664908111a32f
杉山裕紀 <>
2020-08-13 23:16:40 +0900

    upd

--- App.js
+++ App.js
@@ -3,19 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
-
-  _onPress = () => {
-    Alert.alert("ボタンをクリックしました");
-    console.log('click now');
-  }
-
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Button title="button"
-              onPress={() => Alert.alert("click now")}/>
-
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -26,6 +16,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor:  '#fff',
   },
 });
